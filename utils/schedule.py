d3bbb4e0f95129a91da0d8a01c33a0a64c94b550
gTopa <gtopa@stuy.edu>
2017-01-10 16:37:58 -0500

    updating shuold work

--- utils/schedule.py
+++ utils/schedule.py
@@ -3,6 +3,12 @@ import sqlite3
 def createSchedule(schedule, userID):
     db=sqlite3.connect("data/users.db")
     c=db.cursor()
+    c.execute('SELECT userID from schedule')
+
+    hold=c.fetchall()
+    for users in hold):
+        if(users[0]==userID):
+            c.execute('DELETE FROM schedule WHERE userID=="'+userID+'";')
 
     c.execute('INSERT INTO schedule VALUES("'+userID+'", "'+schedule[0]+'", "'+schedule[1]+'", "'+schedule[2]+'", "'+schedule[3]+'", "'+schedule[4]+'", "'+schedule[5]+'", "'+schedule[6]+'", "'+schedule[7]+'", "'+schedule[8]+'", "'+schedule[9]+'");')
 
