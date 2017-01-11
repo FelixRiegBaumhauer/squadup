b55d497ddf9357b64aed1444145cab937f70f73d
Kathy Lau <kathy@mit.edu>
2017-01-11 18:12:54 -0500

    update time

--- utils/locate.py
+++ utils/locate.py
@@ -2,6 +2,7 @@
 #making this show all locations for now
 
 import sqlite3
+from time import gmtime, strftime
 
 def retUsers():
     db=sqlite3.connect("data/users.db")
@@ -13,6 +14,10 @@ def updateLoc(loc, user):
     db=sqlite3.connect("data/users.db")
     c=db.cursor()
     c.execute('UPDATE users SET location = "'+loc+'" WHERE username="'+user+'";')
+
+    showtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())[6:]
+    c.execute('UPDATE users SET time = "' + showtime +'" WHERE username="' + user+ '";')
+    
     db.commit()
     db.close()
     return 0
