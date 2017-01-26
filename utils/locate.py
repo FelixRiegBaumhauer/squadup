61b53e4dd50ad25759e22af43b3655e878e4dcf3
Kathy Lau <kathy@mit.edu>
2017-01-26 01:13:04 -0500

    Update locate.py

--- utils/locate.py
+++ utils/locate.py
@@ -9,14 +9,14 @@ f="data/users.db"
 f2 = open('keys.txt')
 f2 = f2.readlines()
 
-key = f2[0]
+key = f2[0] # get api key
 
 #return users who have a current location
 def retUsers():
     db=sqlite3.connect(f)
     c=db.cursor()
-    c.execute('SELECT * FROM users WHERE time != "";')
-    return c.fetchall()[::-1]
+    c.execute('SELECT * FROM users WHERE time != "";') 
+    return c.fetchall()[::-1] # get in reverse time order
 
 #print retUsers()
 
@@ -27,17 +27,21 @@ def retAllUsers():
     c.execute('SELECT * FROM users;')
     return c.fetchall()[::-1]
 
+#return all friends of users from users table
 def retAllFriends(q):
     db=sqlite3.connect(f)
     c=db.cursor()
-    fList = utils.search.retFriends(q)[0]
+    fList = utils.search.retFriends(q)[0] #list of friend names
     retList = []
+    
     for fr in fList:
         c.execute('SELECT * FROM users WHERE username =="' + fr + '";')
+        
         try:
             retList.append(c.fetchall()[0])
         except:
             pass
+        
     return retList
 
 #the function that updates the entry in the db that correspondes to the users location
@@ -54,7 +58,7 @@ def updateLoc(loc, user):
     db.close()
     return 0
 
-
+#returns current location of user specified
 def retCurrentLocation(user):
     db=sqlite3.connect("data/users.db")
     c=db.cursor()
@@ -63,39 +67,47 @@ def retCurrentLocation(user):
 
 #t : 0 is for friends only
 #t : 1 is for all users
-
+#returns user's location in format of [address, lat, lon]
 def maptesting(user, t):
     randcoordArray = [[40.76727216, -73.99392888], [40.71911552, -74.00666661], [40.71117416, -74.00016545], [40.68382604, -73.97632328], [40.74177603, -74.00149746], [40.69608941, -73.97803415], [40.68676793, -73.95928168], [40.73172428, -74.00674436], [40.72710258, -74.00297088], [40.761628, -73.972924], [40.69239502, -73.99337909], [40.69839895, -73.98068914], [40.71625008, -74.0091059], [40.71542197, -74.01121978], [40.7208736, -73.98085795], [40.722103786686034, -73.99724900722504], [40.71473993, -74.00910627], [40.752062307, -73.9816324043], [40.69089272, -73.99612349], [40.72917025, -73.99810231], [40.75323098, -73.97032517], [40.7489006, -73.97604882], [40.73971301, -73.99456405], [40.76068327096592, -73.9845272898674], [40.7381765, -73.97738662], [40.70905623, -74.01043382], [40.74334935, -74.00681753], [40.70037867, -73.99548059], [40.70277159, -73.99383605], [40.73781509, -73.99994661], [40.71146364, -74.00552427], [40.74195138, -74.00803013], [40.7546011026, -73.971878855], [40.72743423, -73.99379025], [40.69597683, -73.99014892], [40.7284186, -73.98713956], [40.73047309, -73.98672378], [40.7361967, -74.00859207], [40.69196566, -73.9813018], [40.68981035, -73.97493121], [40.697787, -73.973736], [40.688226, -73.979382], [40.69196035, -73.96536851], [40.69327018, -73.97703874], [40.73535398, -74.00483091], [40.72185379, -74.00771779], [40.71870987, -74.0090009], [40.72456089, -73.99565293], [40.72317958, -73.99480012], [40.73226398, -73.99852205]]
     llen = len(randcoordArray)
+    
     if t == 0:
         friends = retAllFriends(user)#utils.search.retFriends(user)[0]
     else:
         friends = retAllUsers()
+        
     coords = []
     lat=''
     lon=''
+    
     for f in friends:
         if t==0:
             currLoc = str(retCurrentLocation(f[1])[0][0])
-            if len(currLoc) > 4:
+            
+            if len(currLoc) > 4: # if not in stuy get inputted location
                 latlon = geo_loc(currLoc.replace(',',' ').replace(' ','+'))
                 lat = latlon['lat']
                 lon = latlon['lng']
                 coords.append([currLoc,lat,lon])
+                
             else:
-                place = utils.schedule.getPeriod(f[1])
+                place = utils.schedule.getPeriod(f[1]) # if in stuy, get class room location
                 updateLoc(str(place), f[1])
                 coords.append(['Stuyvesant', 40.7179464, -74.0139052])
         else:
             currLoc = f[3]
+            
             if len(currLoc) > 4:
                 latlon = geo_loc(currLoc.replace(',',' ').replace(' ','+'))
                 lat = latlon['lat']
                 lon = latlon['lng']
                 coords.append([currLoc,lat,lon])
-            else:
+                
+            else: # if list is not long enough for geoguessr game get random places
                 i = int(random.random() * llen)
                 coords.append(['randplace', randcoordArray[i][0], randcoordArray[i][1]])
+                
     return [lat,lon,coords]
 
 def geo_loc(location):
